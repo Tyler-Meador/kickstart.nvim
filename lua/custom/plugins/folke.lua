e38c09923948b9d7b17fea38357abb3df85c5eb3
Tyler <tmeador13@gmail.com>
2024-08-23 17:12:27 -0500

    modularize plugins

---
+++ lua/custom/plugins/folke.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
