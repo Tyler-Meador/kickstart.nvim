41245b15e553deb36331f4221e3e1b63e08debf1
Tyler <tmeador13@gmail.com>
2024-08-23 20:20:37 -0500

    catppuccin theme

--- lua/custom/plugins/folke.lua
+++ lua/custom/plugins/folke.lua
@@ -3,12 +3,12 @@ return {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+    --init = function()
+    --   vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+    --   -- You can configure highlights by doing something like:
+    --   vim.cmd.hi 'Comment gui=none'
+    --end,
   },
 
   -- Highlight todo, notes, etc in comments
